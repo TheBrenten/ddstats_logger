504b183b611be5acfaa123a203537f83c5adebf7
Alex Wilkerson <alex@uncorrected.com>
2018-05-20 01:48:34 -0500

    added fileio and json capabilities

--- DDGC/Source.cpp
+++ DDGC/Source.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip> 
+#include <vector>
 #include <windows.h>
 #include <string>
 #include <ctime>
 #include <tlhelp32.h>
 #include <tchar.h>
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
+
+// interval in seconds for how often variables are recorded
+const int INTERVAL = 1;
 
 HANDLE hProcHandle = NULL;
 
 uintptr_t GetModuleBaseAddress(DWORD dwProcID, TCHAR *szModuleName);
-// DWORD findDmaAddy(int pointerLevel, HANDLE hProcHandle, DWORD offsets[], DWORD BaseAddress);
-void writeToMemory(HANDLE hProcHandle);
 void collectGameVars(HANDLE hProcHandle);
+void commitVectors();
+void resetVectors();
+void writeLogFile();
 
 string gameName = "Devil Daggers";
 LPCSTR gameWindow = "Devil Daggers";
@@ -24,6 +32,8 @@ bool isGameAvail;
 bool updateOnNextRun;
 bool recording = false;
 
+int recordingCounter = 0;
+
 // testing
 int testSubmitCounter = 0;
 
@@ -32,24 +42,34 @@ float oldInGameTimer;
 float inGameTimer;
 DWORD inGameTimerBaseAddress = 0x001F30C0;
 DWORD inGameTimerOffset = 0x1A0;
+vector <float> inGameTimerVector;
+int playerID;
+DWORD playerIDBaseAddress = 0x001F30C0;
+DWORD playerIDOffset = 0x5C;
 int gems;
 DWORD gemsBaseAddress = 0x001F30C0;
 DWORD gemsOffset = 0x1C0;
+vector <int> gemsVector;
 int homingDaggers;
 DWORD homingDaggersBaseAddress = 0x001F8084;
 DWORD homingDaggersOffset = 0x224;
+vector <int> homingDaggersVector;
 int daggersFired;
 DWORD daggersFiredBaseAddress = 0x001F30C0;
 DWORD daggersFiredOffset = 0x1B4;
+vector <int> daggersFiredVector;
 int daggersHit;
 DWORD daggersHitBaseAddress = 0x001F30C0;
 DWORD daggersHitOffset = 0x1B8;
+vector <int> daggersHitVector;
 int enemiesAlive;
 DWORD enemiesAliveBaseAddress = 0x001F30C0;
 DWORD enemiesAliveOffset = 0x1FC;
+vector <int> enemiesAliveVector;
 int enemiesKilled;
 DWORD enemiesKilledBaseAddress = 0x001F30C0;
 DWORD enemiesKilledOffset = 0x1BC;
+vector <int> enemiesKilledVector;
 int deathType;
 DWORD deathTypeBaseAddress = 0x001F30C0;
 DWORD deathTypeHitOffset = 0x1C4;
@@ -112,9 +132,10 @@ int main() {
 				if (daggersFired > 0.0)
 					cout << "Accuracy: " << setprecision(4) << ((float) daggersHit / (float) daggersFired) * 100.0 << "%" << endl;
 				else
-					cout << "Accuracy: 0.0%" << endl;
+					cout << "Accuracy: 0%" << endl;
 				cout << "Enemies Alive: " << enemiesAlive << endl;
 				cout << "Enemies Killed: " << enemiesKilled << endl;
+				cout << "In Game Timer Vector Size: " << inGameTimerVector.size() << endl;
 				cout << "Submissions: " << testSubmitCounter << endl;
 				cout << endl << "[F10] Exit" << endl;
 				updateOnNextRun = false;
@@ -127,10 +148,15 @@ int main() {
 				collectGameVars(hProcHandle);
 				if (alive && inGameTimer > 0 && !isReplay) {
 					recording = true;
+					if (recordingCounter < inGameTimer) {
+						commitVectors();
+					}
 				}
 				if (!alive && recording == true) {
 					recording = false;
 					// submit
+					writeLogFile();
+					resetVectors(); // reset after submit
 					testSubmitCounter++;
 				}
 
@@ -153,22 +179,27 @@ int main() {
 	return ERROR_SUCCESS;
 }
 
-//DWORD findDmaAddy(int pointerLevel, HANDLE hProcHandle, DWORD offsets[], DWORD BaseAddress) {
-//	DWORD pointer = BaseAddress;
-//	DWORD pTemp;
-//
-//	DWORD pointerAddr;
-//	for (int i = 0; i < pointerLevel; i++) {
-//		if (i == 0) {
-//			ReadProcessMemory(hProcHandle, (LPCVOID)pointer, &pTemp, sizeof(pTemp), NULL);
-//		}
-//
-//		pointerAddr = pTemp + offsets[i];
-//		ReadProcessMemory(hProcHandle, (LPCVOID)pointerAddr, &pTemp, sizeof(pTemp), NULL);
-//		cout << "test" << endl;
-//	}
-//	return pointerAddr;
-//}
+void commitVectors() {
+	recordingCounter += INTERVAL;
+	inGameTimerVector.push_back(inGameTimer);
+	gemsVector.push_back(gems);
+	homingDaggersVector.push_back(homingDaggers);
+	daggersFiredVector.push_back(daggersFired);
+	daggersHitVector.push_back(daggersHit);
+	enemiesAliveVector.push_back(enemiesAlive);
+	enemiesKilledVector.push_back(enemiesKilled);
+}
+
+void resetVectors() {
+	recordingCounter = 0;
+	inGameTimerVector.clear();
+	gemsVector.clear();
+	homingDaggersVector.clear();
+	daggersFiredVector.clear();
+	daggersHitVector.clear();
+	enemiesAliveVector.clear();
+	enemiesKilledVector.clear();
+}
 
 void collectGameVars(HANDLE hProcHandle) {
 	DWORD pointer;
@@ -186,6 +217,8 @@ void collectGameVars(HANDLE hProcHandle) {
 		ReadProcessMemory(hProcHandle, (LPCVOID)pointerAddr, &inGameTimer, sizeof(inGameTimer), NULL);
 		if ((inGameTimer < oldInGameTimer) && recording) {
 			// submit, but use oldInGameTimer var instead of new... then continue.
+			writeLogFile();
+			resetVectors(); // reset after submit
 			testSubmitCounter++;
 		}
 	}
@@ -251,35 +284,6 @@ void collectGameVars(HANDLE hProcHandle) {
 	}
 }
 
-
-void writeToMemory(HANDLE hProcHandle) {
-	DWORD pointer = exeBaseAddress + gemsBaseAddress;
-	DWORD pTemp;
-
-	DWORD pointerAddr;
-	if (gemStatus) {
-		// addressToWrite = findDmaAddy(1, hProcHandle, gemOffset, gemBaseAddress);
-		if (!ReadProcessMemory(hProcHandle, (LPCVOID)pointer, &pTemp, sizeof(pTemp), NULL))
-			cout << "Failed to read address for gem counter." << endl;
-
-		pointerAddr = pTemp + 0x1C0;
-		ReadProcessMemory(hProcHandle, (LPCVOID)pointerAddr, &gems, sizeof(gems), NULL);
-		gemOnScreenValue = gems;
-
-		if (!ReadProcessMemory(hProcHandle, (LPCVOID)pointer, &pTemp, sizeof(pTemp), NULL))
-			cout << "Failed to read address for player best." << endl;
-
-		string testing = "LukeNukemRR";
-		SIZE_T sizeOfTesting = testing.length() + 1;
-		pointerAddr = pTemp + 0x30C;
-
-		//if (!WriteProcessMemory(hProcHandle, (BYTE*)pointerAddr, &gemOnScreenValue, sizeof(gemOnScreenValue), NULL))
-		//	cout << "Failed to write process memory." << endl;
-		if (!WriteProcessMemory(hProcHandle, (BYTE*)pointerAddr, testing.c_str(), testing.length()+1, NULL))
-			cout << "Failed to write process memory." << endl;
-	}
-}
-
 uintptr_t GetModuleBaseAddress(DWORD dwProcID, TCHAR *szModuleName) {
 	uintptr_t ModuleBaseAddress = 0;
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, dwProcID);
@@ -297,4 +301,24 @@ uintptr_t GetModuleBaseAddress(DWORD dwProcID, TCHAR *szModuleName) {
 		CloseHandle(hSnapshot);
 	}
 	return ModuleBaseAddress;
+}
+
+void writeLogFile() {
+	float accuracy;
+	if (daggersFired > 0.0)
+		accuracy = ((float)daggersHit / (float)daggersFired) * 100.0;
+	else
+		accuracy = 0.0;
+	json log = {
+		{ "inGameTimer", inGameTimer },
+		{"gems", gems},
+		{"homingDaggers", homingDaggers},
+		{"daggersFired", daggersFired},
+		{"daggersHit", daggersHit},
+		{"accuracy", accuracy},
+		{"enemiesAlive", enemiesAlive},
+		{"enemiesKilled", enemiesKilled}
+	};
+	ofstream o(to_string(clock()) + ".json");
+	o << setw(4) << << setprecision(4) << log << endl;
 }
